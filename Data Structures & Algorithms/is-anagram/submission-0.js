c6ae662f06771f965f279990cd8ece0840306c4d
Salisu Ibrahim <salisu.ibrah91@gmail.com>
2026-07-22 07:31:13 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+        let s_sorted = s.split("").sort().join("");
+        let t_sorted = t.split("").sort().join("");
+        if (s_sorted === t_sorted) {
+            return true;
+        }
+        else return false;
+    }
+}
